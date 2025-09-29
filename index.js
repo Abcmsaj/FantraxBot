5f8363fbf82e754d7fcfa7a64f2404dac622798e
Alex Dixon <alex.dixon@cloudbooking.com>
2025-09-29 10:53:21 +0100

    Swap to "clientReady"

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.login(token);
 // ----------------
 // Ready
 // ----------------
-client.once('ready', () => {
+client.once('clientReady', () => {
     // Uncomment to delete all commands from the bot and guild
     const guild = client.guilds.cache.get(guildId);
     // client.application.commands.set([]);
